94fcfe9a3c1c52e344b9ec8939789ef8b99f3f90
David Kananda <davidkananda@gmail.com>
2018-10-21 23:51:01 +0100

    add styles loaders and configs

--- webpack.config.js
+++ webpack.config.js
@@ -3,13 +3,17 @@ const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const webpack = require("webpack");
 
 module.exports = {
-  entry: ["./src/index.js"],
+  entry: ["./src/index.js", "./src/styles/main.scss"],
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "./public/dist")
   },
   module: {
     rules: [
+      {
+        test: /\.scss/,
+        loader: ExtractTextPlugin.extract(["css-loader", "sass-loader"])
+      },
       {
         test: /\.js$/,
         exclude: /node_modules/,
@@ -28,6 +32,7 @@ module.exports = {
     watchContentBase: true
   },
   plugins: [
+    new ExtractTextPlugin("bundle.css"),
     new webpack.DefinePlugin({
       "process.env.NODE_ENV": JSON.stringify("development")
     }),
