d78217cda2bf6e72bd14493cf1e52fc1acb76606
David Kananda <davidkananda@gmail.com>
2018-10-21 13:21:34 +0100

    Pass test and refactor renamed test file and imports added components

---
+++ src/components/WeatherBoard.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const WeatherDashboard = ({ data }) => {
+    return (
+      <div>
+        <header>
+          <h1>5-Day Weather Forecast</h1>
+        </header>
+        <section>
+          <div>
+            Days go here
+          </div>
+        </section>
+      </div>
+    );
+};
+
+export default WeatherDashboard;
