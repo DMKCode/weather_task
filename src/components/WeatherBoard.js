642fccd0d9e2a2a38fa21a1a90fe605690622034
David Kananda <davidkananda@gmail.com>
2018-10-21 16:18:00 +0100

    connect weather dashboard

--- src/components/WeatherBoard.js
+++ src/components/WeatherBoard.js
@@ -1,20 +1,28 @@
-import React, { PureComponent } from "react";
+import React, { PureComponent } from 'react';
+import { connect } from 'react-redux';
+
+import WeatherForecast from './WeatherForecast';
 
 class WeatherDashboard extends PureComponent {
   render() {
+    const { list } = this.props;
     return (
       <div>
         <header>
           <h1>5-Day Weather Forecast</h1>
         </header>
         <section>
-          <div>
-            Days go here
-          </div>
+          <WeatherForecast forecasts={list} />
         </section>
       </div>
     );
   }
 };
 
-export default WeatherDashboard;
+const mapStateToProps = (state) => {
+  return {
+    list: state.data.list || {}
+  };
+};
+
+export default connect(mapStateToProps)(WeatherDashboard);
