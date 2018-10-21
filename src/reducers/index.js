3b6b3b4c7b61b0479c0a255bd7a9709efadc8e84
David Kananda <davidkananda@gmail.com>
2018-10-21 16:17:37 +0100

    Refactor initialState moved initialState to store

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,13 +1,6 @@
 import { REQUEST_DATA, REQUEST_DATA_SUCCESS, REQUEST_DATA_FAILURE } from '../actions';
-import sampleData from './sample_data';
 
-const initialState = {
-  data: sampleData,
-  source: 'api',
-  status: null
-};
-
-export default function reducer(state = initialState, action) {
+export default function reducer(state = {}, action) {
   switch (action.type) {
     case REQUEST_DATA: {
       return {
--- src/store/index.js
+++ src/store/index.js
@@ -4,9 +4,15 @@ import { createLogger } from "redux-logger";
 
 import reducer from '../reducers';
 import rootSaga from '../sagas';
+import sampleData from '../reducers/sample_data';
 
+const initialState = {
+  data: sampleData,
+  source: 'api',
+  status: null
+};
 
-const configureStore = (preloadedState = {}) => {
+const configureStore = (preloadedState = initialState) => {
   const sagaMiddleware = createSagaMiddleware();
   const middleware = applyMiddleware(sagaMiddleware, createLogger());
   const store = createStore(reducer, preloadedState,
